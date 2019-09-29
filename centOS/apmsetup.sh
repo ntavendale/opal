266be2469631fea7c56827d0f97e9c1953deb99e
ntavendale <nigel.tavendale@allthingssyslog.com>
2019-09-29 11:58:08 -0600

    Pull down es

--- centOS/apmsetup.sh
+++ centOS/apmsetup.sh
@@ -1,1 +1,16 @@
 #!bin/bash
+rpm --import https://artifacts.elastic.co/GPG-KEY-elasticsearch
+
+cat > $file_location <<EOF
+[elasticsearch-7.x]
+name=Elasticsearch repository for 7.x packages
+baseurl=https://artifacts.elastic.co/packages/7.x/yum
+gpgcheck=1
+gpgkey=https://artifacts.elastic.co/GPG-KEY-elasticsearch
+enabled=1
+autorefresh=1
+type=rpm-md
+EOF
+
+yum install elasticsearch
+chkconfig --add elasticsearch
